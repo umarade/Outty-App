b2b28de67b5e5a7f46f1dbefc64c37ce48d0ebe1
BillRoan <simobill2002@gmail.com>
2026-03-30 21:27:10 -0400

    Added screen folder into App

--- App/App.tsx
+++ App/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 export type RootStackParamList = {
   Login: undefined;
