4393e1ecdd6a1d02461bc257c9c163c7820b6f50
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-04-19 23:54:48 -0400

    integrated frontend with backend api- removed hardcoding, replaced with real profile call instead

--- App/server/index.js
+++ App/server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 // @ts-ignore
 const profileRoute = require('./src/routes/profileRoutes')
